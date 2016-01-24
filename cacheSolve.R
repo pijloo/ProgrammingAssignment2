e7bf0a85b932c908e60be09e8c73bb4db6f5fedc
Pijloo <sten.pijloo@gmail.com>
2016-01-24 17:44:29 +0100

    Create cacheSolve.R

---
+++ cacheSolve.R
@@ -0,0 +1,4 @@
+##cacheSolve
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix.R before.
+## If the inverse has already been calculated( and the matrix has not changed), 
+## then the cachesolve should retrive the inverse from cache.
